8ad4cab306a56925b90f78c515e72e1198d7bd9c
ayushsingh212 <ayurajput249@gmail.com>
2025-08-05 06:15:49 +0530

    ca

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/TheNimantran.com/', // 👈 Use your repo name here
+});
