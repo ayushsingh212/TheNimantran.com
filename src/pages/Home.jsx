ff59688f426e6b0d7fd2136672a89b436be1fa56
Ansh Tripathi <ansh.tripathi17012006@gmail.com>
2025-08-07 05:20:06 +0530

    Add: updated homepage layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,26 +49,26 @@ const categories = [
   { title: "Baby Shower", price: "₹180", image: babyShowerImg, category: "baby-shower" },
   { title: "Festive Greetings", price: "₹100", image: festiveImg, to: "festive-greetings" },
   { title: "Corporate", price: "₹150", image: corporate1, category: "corporate" },
- 
+
 ];
 
 
 function Home() {
 
-   const [category, setCategory] = useState('wedding')
+  const [category, setCategory] = useState('wedding')
 
 
   return (
-    <div className="space-y-16 px-4 py-8 font-serif bg-white text-gray-800 relative">
-      
-      <WelcomeBanner/>
+    <div className="space-y-10 px-4 pt-8 font-serif bg-[#f4f0d4] text-gray-800 relative">
 
-<Carousel
+      <WelcomeBanner />
+
+      <Carousel
         swipeable
         draggable
         responsive={responsive}
         infinite
-         autoPlay = {false}
+        autoPlay={false}
         autoPlaySpeed={2500}
         keyBoardControl
         transitionDuration={500}
@@ -76,59 +76,67 @@ function Home() {
         removeArrowOnDeviceType={["tablet", "mobile"]}
         itemClass="carousel-item-padding-40-px"
       >
-        {images.map((img) => (
-          <img
-            key={img.id}
-            src={img.link}
-            alt={`Slide ${img.id}`}
-            className="w-full h-[320px] object-cover rounded-lg shadow-lg"
-          />
-        ))}
+        <div className="w-full h-[294.5px] md:h-[330px] overflow-hidden border-[2vw] md:border-[1vw] border-[#E9A319] rounded-xl">
+          {images.map((img) => (
+            <img
+              key={img.id}
+              src={img.link}
+              alt={`Slide ${img.id}`}
+              className="w-full h-full object-cover object-[17.5%_0%] md:object-[center]"
+              loading="eager"
+              decoding="async"
+              fetchpriority="high"
+            />
+          ))}
+        </div>
       </Carousel>
 
 
-     
 
 
-    
 
-<section className="px-4 md:px-20 py-10 bg-background">
-  <h2 className="text-3xl font-bold text-center mb-8 text-heading">Our Categories</h2>
 
-  <div className="flex flex-wrap justify-center gap-6">
-    {categories.map((card, index) => (
-      <button
-        onClick={ ()=>
-          setCategory(card.category)
-        }
-        key={index}
-        className="flex flex-col items-center text-center"
-      >
-        <div className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 rounded-full overflow-hidden shadow-md hover:shadow-xl transition">
-          <img
-            src={card.image}
-            alt={card.title}
-            className="w-full h-full object-cover"
-          />
+
+      <section className="px-4 md:px-10 ">
+        <h2 className="text-4xl md:text-5xl font-semibold text-center md:mb-[2.4vw] mb-[9vw]  text-heading underline decoration-[#F3C623] decoration-[6px]">Our Categories</h2>
+
+        <div className="flex flex-wrap justify-center gap-6">
+          {categories.map((card, index) => (
+            <button
+              onClick={() =>
+                setCategory(card.category)
+              }
+              key={index}
+              className="md:w-[200px] md:h-[220px] w-[150px] h-auto pt-5 border-[3px] md:border-[2px] border-[#F3C623] rounded-xl flex flex-col items-center text-center bg-white rounded-lg hover:shadow-[5px_5px_9px_#F3C623]"
+            >
+              <div className="w-20 h-20 bg-purple-300 sm:w-24 sm:h-24 md:w-28 md:h-28 rounded-full overflow-hidden">
+                <img
+                  src={card.image}
+                  alt={card.title}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div className="md:h-[5vw] h-[22vw] w-[12vw]  flex items-center justify-center">
+                <h3 className="mt-2 text-xl font-semibold">{card.title}</h3>
+              </div>
+
+            </button>
+          ))}
         </div>
-        <h3 className="mt-2 text-xs sm:text-sm md:text-base font-semibold">{card.title}</h3>
-      </button>
-    ))}
-  </div>
-</section>
+      </section>
 
-<CardPage  category={category} />
+      <CardPage category={category} />
 
 
 
-    
-      
-      <PopularCards/>
-      <TrendingCards/>
+
+
+      <PopularCards />
+      <TrendingCards />
 
 
 
-      
+
       <OurServices />
       <WhyChooseUs />
     </div>
